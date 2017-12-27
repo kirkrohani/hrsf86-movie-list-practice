58730bce66279321af9f3fcb2c878ac4c3bae89d
Andrew Kang <andrewsjkang@gmail.com>
2017-12-27 12:59:08 -0800

    Implement rendering for list of movies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import MovieItem from './components/Movie.jsx'
 
+//// HARD CODE DATA ////
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 class MovieList extends React.Component {
   constructor() {
     super();
+    this.state = {
+      movies: movies
+    };
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        { this.state.movies.map(movie => {
+            return <MovieItem movie={ movie } key={ movie.title }/>
+          })
+        }
+      </div>
     )
   }
 }
 
-ReactDOM.render( <MovieList />, document.getElementById('app'));
+ReactDOM.render(<MovieList />, document.getElementById('app'));
