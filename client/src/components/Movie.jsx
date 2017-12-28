153e459b0a936c7b403166339c6866d635705f58
Nick Forster <nforster24@gmail.com>
2017-12-27 21:47:18 -0500

    more refactoring

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -30,56 +30,54 @@ class Movie extends React.Component {
   	var button = null;
   	var displayDiv = null;
 
-  	  if (isWatched) {
-  	  	 button = <button className = 'watchedButton' onClick = {() => this.toggleWatchButton()}> Watched </button> ; 
-  	  } else {
-  		button = <button onClick = {() => this.toggleWatchButton()}> To Watch </button> ;   	
-  	  }
+  	if (isWatched) {
+  	  button = <button className = 'watchedButton' onClick = {() => this.toggleWatchButton()}> Watched </button> ; 
+  	 } else {
+  	  button = <button onClick = {() => this.toggleWatchButton()}> To Watch </button> ;   	
+  	}
 
-  	  if (isClicked) {
-  	  	displayDiv = <div className = 'movieEntry'> 
-  	  	  <div onClick = {() => this.renderMovieDetails()}> 
-	        {this.props.movie.title}
-	      </div> 
-	      <div className = 'watchedDiv'> {button} </div>
-	      <MovieDetails movie = {this.props.movie}/> 
-	    </div>
-  	  } else {
-  	  	displayDiv = <div className = 'movieEntry'>
-  	  	<div onClick = {() => this.renderMovieDetails()}> {this.props.movie.title} </div> 
-  	  	<div className = 'watchedDiv'> {button} </div>
-	    </div>
-  	  }
-
-
+	if (isClicked) {
+	  displayDiv = 
+	  <div className = 'movieEntry'> 
+	    <div onClick = {() => this.renderMovieDetails()}> {this.props.movie.title} </div> 
+        <div className = 'watchedDiv'> {button} </div>
+        <MovieDetails movie = {this.props.movie}/> 
+      </div> 
+	 } else {
+	   displayDiv = 
+	   <div className = 'movieEntry'>
+	     <div onClick = {() => this.renderMovieDetails()}> {this.props.movie.title} </div> 
+	  	 <div className = 'watchedDiv'> {button} </div>
+       </div>
+	 }
 
   	if (this.props.watchedMoviesShown === 'neither') {
   	  return (displayDiv)
   	}
+
   	if (this.props.watchedMoviesShown === 'yes') {
-	  	if (isWatched) {
-	  	  return (
-	  	    displayDiv
-	  	  )
-	    } else {
-	    	return (
-	    		<div> </div>
-	    	)
-	    }
+	  if (isWatched) {
+	    return (
+	  	  displayDiv
+	  	)
+	  } else {
+        return (
+          <div> </div>
+        )
+	  }
     } else {
     	// render all movies which havent been watched (isWatched: false)
-    	if (!isWatched) {
-	  	  return (
-	  	    displayDiv
-	  	  )
-	    } else {
-	    	return (
-	    		<div> </div>
-	    	)
-	    }	
+      if (!isWatched) {
+	    return (
+	  	  displayDiv
+	  	)
+	  } else {
+    	return (
+    		<div> </div>
+    	)
+      }	
     }
-
-    }
+  }
 }
 
 export default Movie;
