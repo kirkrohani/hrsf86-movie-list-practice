a3992312be90b2ba621df340c4ec874986849e5f
Dillon Lin <dillonlintrade@outlook.com>
2017-12-27 16:31:33 -0800

    debugging Level 4 - this is painful

---
+++ database/index.js
@@ -0,0 +1,47 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'movieList'
+});
+
+connection.connect(function(error){
+	if(error) {throw error;}
+	console.log('CONNECTED TO THE DATABASE!');
+});
+
+movies = {
+	get: (callback) => {
+		var query = 'SELECT * FROM movies';
+		connection.query(query, function(error, result) {
+			callback(error, result);
+		});
+	},
+
+	post: (parameters, callback) => {
+		var query = 'INSERT INTO movies (title, release_date, overview, popularity, vote_average, watched) VALUES ?';
+		connection.query(query, parameters, function(error, result) {
+			callback(error, result);
+		})
+	},
+
+	postNewMovie: (parameters, callback) => {
+		var query = 'INSERT INTO movies (title, watched) VALUES (?, false)';
+		connection.query(query, parameters, function(error, result) {
+			callback(error, result);
+		})
+	},
+
+	toggleWatched: (parameters, callback) => {
+		var query = 'UPDATE movies SET watched = NOT watched WHERE (title = ?)';
+		connection.query(query, parameters, function(error, result) {
+			if(error) {throw error;}
+			callback(result);
+		})
+	}
+}
+
+module.exports.connection = connection;
+module.exports.movies = movies;
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const movieAPI = require('../lib/movieAPI.js');
-
+const db = require('../database/index.js');
+const Promise = require('bluebird');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,37 +12,69 @@ app.use(bodyParser.json());
 
 
 // Our Initial Data
-var movies = [];
 
 // OUR GET AND POST RESPONSES
 app.get('/load', function(req, res) {
 	movieAPI.getRequest(function(data) {
+		var movies = [];
 		movies = data.results
 		movies.forEach((movie) => {
 			movie.watched = false;
 		});
-		res.send(movies)
+		var sqlMovieList = [];
+		movies.forEach((movie) => {
+			var sqlMovie = [];
+			sqlMovie.push(movie.title, movie.release_date, movie.overview, movie.popularity, movie.vote_average, movie.watched);
+			sqlMovieList.push(sqlMovie);
+		})
+		var parameters = [sqlMovieList];
+		db.movies.post(parameters, function(error, result) {
+			if(error) {throw error;}
+			res.send(movies);
+		});
 	});
 });
 
 app.get('/messages', function(req, res) {
-	res.send(movies);
+	db.movies.get(function(error, result) {
+		res.send(result);
+	})
 });
 
 app.post('/messages', function(req, res) {
 	var matchingArray = [];
-	for(var i = 0; i < movies.length; i++) {
-		matchingArray.push(movies[i].title);
-	}
-	if(matchingArray.indexOf(req.body.title) >= 0) {
-		movies[matchingArray.indexOf(req.body.title)].watched = !movies[matchingArray.indexOf(req.body.title)].watched;
-		res.json(movies);
-	} else {
-		req.body.watched = false;
-		movies.push(req.body);
-		res.json(movies);
-	}
-	
+	const myPromise = new Promise((resolve, reject) => {
+		db.movies.get(function(error, result) {
+			error ? reject(error) : resolve(result);
+		});	
+	}); 
+	return myPromise.then(function(result) {
+		result.forEach((movie) => {matchingArray.push(movie.title)});
+		if(matchingArray.indexOf(req.body.title) >= 0) {
+			var parameter = [req.body.title]
+			db.movies.toggleWatched(parameter, function(error, result) {
+				if(error) {throw error;}
+				console.log('THIS IS THE RESULT', JSON.stringify(result));
+		})
+			} else {
+				var parameters = [req.body.title]
+				db.movies.postNewMovie(parameters, function(error, result) {
+					if(error) {throw error;}
+					console.log(JSON.stringify(result));
+				});
+			}
+		})
+	// for(var i = 0; i < movies.length; i++) {
+	// 	matchingArray.push(movies[i].title);
+	// }
+	// if(matchingArray.indexOf(req.body.title) >= 0) {
+	// 	movies[matchingArray.indexOf(req.body.title)].watched = !movies[matchingArray.indexOf(req.body.title)].watched;
+	// 	res.json(movies);
+	// } else {
+	// 	req.body.watched = false;
+	// 	movies.push(req.body);
+	// 	res.json(movies);
+	// }
 });
 
 
