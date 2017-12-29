2d9cc445ff572c0b511cfd53e8fce2a19c7bc302
Your Name <williamputnamjr@gmail.com>
2017-12-29 11:13:03 -0500

    need to work on css after doing database portion

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -3,7 +3,7 @@ import ReactDOM  from 'react-dom';
 
 const Movie = (props) => (
 	<div>{props.movies.map((movie) => {
-		return <div key={movie.title}>{movie.title}
+		return <div key={movie.title}>{movie.title} {movie.overview}
 	    </div>
 	})}</div>
 );
