1751fcba21ebb5386e0b7e20faf546ce24508c8e
Mark Arce <mark.arce@gmail.com>
2017-12-28 23:21:48 -0800

    modified database to be similar to movie database data

--- database/index.js
+++ database/index.js
@@ -1,9 +1,33 @@
 module.exports =  {
   movieList: [
-            {title: 'Mean Girls'},
-            {title: 'Hackers'},
-            {title: 'The Grey'},
-            {title: 'Sunshine'},
-            {title: 'Ex Machina'},
+            {id: '1', original_title: 'Mean Girls', release_date: '2004-01-01', popularity:'80', vote_average: '4.5'},
+            {id: '2', original_title: 'Hackers', release_date: '1998-01-01', popularity:'13.37', vote_average: '4.5'},
+            {id: '3', original_title: 'The Grey', release_date: '2006-01-01', popularity:'20', vote_average: '4.5'},
+            {id: '4', original_title: 'Sunshine', release_date: '2009-01-01', popularity:'65', vote_average: '4.5'},
+            {id: '5', original_title: 'Ex Machina', release_date: '2015-01-01', popularity:'88', vote_average: '4.5'}
           ]
 };
+
+
+// {
+//   "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
+//   "adult": false,
+//   "overview": "Jack Reacher must uncover the truth behind a major government conspiracy in order to clear his name. On the run as a fugitive from the law, Reacher uncovers a potential secret from his past that could change his life forever.",
+//   "release_date": "2016-10-19",
+//   "genre_ids": [
+//     53,
+//     28,
+//     80,
+//     18,
+//     9648
+//   ],
+//   "id": 343611,
+//   "original_title": "Jack Reacher: Never Go Back",
+//   "original_language": "en",
+//   "title": "Jack Reacher: Never Go Back",
+//   "backdrop_path": "/4ynQYtSEuU5hyipcGkfD6ncwtwz.jpg",
+//   "popularity": 26.818468,
+//   "vote_count": 201,
+//   "video": false,
+//   "vote_average": 4.19
+// }
