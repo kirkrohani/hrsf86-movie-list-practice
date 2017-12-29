66ea834d736da882b0b2f93bcf0ec7790c5781d5
acenine <acenine@gmx.com>
2017-12-28 17:44:57 -0800

    Make get request work

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,14 +3,15 @@ import ReactDOM  from 'react-dom';
 import Search from './components/Search.jsx';
 import AddMovie from './components/AddMovie.jsx';
 import Movie from './components/Movie.jsx';
+import axios from 'axios';
 
 
 class MovieList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentMovieList: props.movies,
-      // query: 'cat'
+      currentMovieList: [],
+      server: 'http://127.0.0.1:3000'
     }
   }
 
@@ -19,6 +20,27 @@ class MovieList extends React.Component {
   //     query: query
   //   });
   // }
+  componentDidMount() {
+    // fetch(this.state.server)
+    //   .then(
+    //     (result) => {
+    //       console.log('Get request succeeded :)')
+    //        var stuff = result.body.getReader();
+    //       // this.setState({
+    //       //   currentMovieList:result
+    //       // });
+    //     },
+    //     (error) => {
+    //       console.log('Get request failed :(')
+    //     }
+    //   )
+    axios.get(this.state.server + "/movies")
+      .then(res => {
+        this.setState({
+          currentMovieList: res.data
+        });
+      });
+  }
 
   filterMLbyQuery(query) {
     var filteredList = this.state.currentMovieList.filter((movie) => {
@@ -48,11 +70,4 @@ class MovieList extends React.Component {
     )
   }
 }
-window.movies = [
-  {title: 'Mean Girls', description : 'descrptive stuff'},
-  {title: 'Hackers', description : 'descrptive stuff'},
-  {title: 'The Grey', description : 'descrptive stuff'},
-  {title: 'Sunshine', description : 'descrptive stuff'},
-  {title: 'Ex Machina', description : 'descrptive stuff'},
-];
-ReactDOM.render( <MovieList movies = {window.movies}/>, document.getElementById('app'));
+ReactDOM.render( <MovieList/>, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+var movies = [
+  {title: 'Mean Girls', description : 'descrptive stuff'},
+  {title: 'Hackers', description : 'descrptive stuff'},
+  {title: 'The Grey', description : 'descrptive stuff'},
+  {title: 'Sunshine', description : 'descrptive stuff'},
+  {title: 'Ex Machina', description : 'descrptive stuff'},
+];
+app.get('/movies', function(req, res) {
+  res.json((movies))
+})
+// app.post()
 app.listen(3000, function () { console.log('MovieList app listening on port 3000!') });
 
 
